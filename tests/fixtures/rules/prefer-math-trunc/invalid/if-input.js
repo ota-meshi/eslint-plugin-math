487855e4697f2b410319a5f1770b465f7f288de4
yosuke ota <otameshiyo23@gmail.com>
2024-05-26 17:28:54 +0900

    add test cases

--- tests/fixtures/rules/prefer-math-trunc/invalid/if-input.js
+++ tests/fixtures/rules/prefer-math-trunc/invalid/if-input.js
@@ -1,21 +1,21 @@
 /* {} */
 if (n >= 0) {
-    x = Math.floor(n);
+  x = Math.floor(n);
 } else {
-    x = Math.ceil(n);
+  x = Math.ceil(n);
 }
 if (n > 0) {
-    x = Math.floor(n);
+  x = Math.floor(n);
 } else {
-    x = Math.ceil(n);
+  x = Math.ceil(n);
 }
 if (n <= 0) {
-    x = Math.ceil(n);
+  x = Math.ceil(n);
 } else {
-    x = Math.floor(n);
+  x = Math.floor(n);
 }
 if (n < 0) {
-    x = Math.ceil(n);
+  x = Math.ceil(n);
 } else {
-    x = Math.floor(n);
+  x = Math.floor(n);
 }
