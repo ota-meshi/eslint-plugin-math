e497bb639ef0cec5100bb80058e8032feed55b50
yosuke ota <otameshiyo23@gmail.com>
2025-06-14 21:55:35 +0900

    feat: refactor to use eslint-type-tracer

--- src/utils/type-checker/object-type-checker.ts
+++ src/utils/type-checker/object-type-checker.ts
@@ -1,27 +1,16 @@
-import type { ObjectTypeChecker } from "./utils";
 import type { Rule } from "eslint";
-import { buildObjectTypeCheckerForES } from "./object-type-checker-for-es";
-import { buildObjectTypeCheckerForTS } from "./object-type-checker-for-ts";
+import type { TypeChecker } from "eslint-type-tracer";
+import { buildTypeChecker } from "eslint-type-tracer";
 
-export type ObjectTypeCheckerOptions = {
-  pluginNamespace: string | string[];
-};
 /**
  * Build object type checker.
  * @param context The rule context.
  * @param aggressiveResult The value to return if the type cannot be determined.
  * @returns Returns an object type checker.
  */
-export function buildObjectTypeChecker(
-  context: Rule.RuleContext,
-  options: ObjectTypeCheckerOptions,
-): ObjectTypeChecker {
-  const aggressiveOption = getAggressiveOption(context, options);
-  const aggressiveResult = aggressiveOption ? "aggressive" : false;
-  return (
-    buildObjectTypeCheckerForTS(context, aggressiveResult) ||
-    buildObjectTypeCheckerForES(context, aggressiveResult)
-  );
+export function buildObjectTypeChecker(context: Rule.RuleContext): TypeChecker {
+  const aggressiveOption = getAggressiveOption(context);
+  return buildTypeChecker(context.sourceCode, { aggressive: aggressiveOption });
 }
 
 /**
@@ -29,22 +18,17 @@ export function buildObjectTypeChecker(
  * @param context The rule context.
  * @returns The gotten `aggressive` option value.
  */
-function getAggressiveOption(
-  context: Rule.RuleContext,
-  option: ObjectTypeCheckerOptions,
-): boolean {
+function getAggressiveOption(context: Rule.RuleContext): boolean {
   const options = context.options[0];
   if (options && typeof options.aggressive === "boolean") {
     return options.aggressive;
   }
 
-  for (const ns of [option.pluginNamespace].flat()) {
-    const globalOptions = context.settings[ns] as
-      | Record<string, unknown>
-      | undefined;
-    if (globalOptions && typeof globalOptions.aggressive === "boolean") {
-      return globalOptions.aggressive;
-    }
+  const globalOptions = context.settings.math as
+    | Record<string, unknown>
+    | undefined;
+  if (globalOptions && typeof globalOptions.aggressive === "boolean") {
+    return globalOptions.aggressive;
   }
 
   return false;
